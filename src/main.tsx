7b5f741858f49ee4e8023010d4a88c75e10320c9
MaxIliv <max.r.iliv@gmail.com>
2025-03-10 21:48:43 +0200

    Add Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,41 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
 
 import './index.css'
 import App from './App.js'
 import ProductsApp from './ProductsApp'
 import TodoListComponent from './TodoList'
 import UsersList from './Users';
+import HomePage from './pages/HomePage';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
-      <ProductsApp />
-      <TodoListComponent />
-      <Provider store={store}>
-        <UsersList />
-      </Provider>
+      <BrowserRouter>
+        <div>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/game">Game</NavLink>
+          <NavLink to="/todo">Todo</NavLink>
+          <NavLink to="/products">Products</NavLink>
+          <NavLink to="/users">Users</NavLink>
+        </div>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/game" element={<App />} />
+          <Route path="/todo" element={<TodoListComponent />} />
+          <Route path="/products" element={<ProductsApp />} />
+          <Route path="/users" element={
+            <Provider store={store}>
+              <UsersList />
+            </Provider>
+          } />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>,
   )
 }
