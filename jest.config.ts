20072dc4b5dd95614486f526b67785697a704308
MaxIliv <max.r.iliv@gmail.com>
2025-05-13 20:08:04 +0300

    test: e2e add playwright

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type {Config} from 'jest';
 
 export default async (): Promise<Config> => {
   return {
+    rootDir: '__tests__',
     verbose: true,
     coverageProvider: 'v8',
     testEnvironment: 'jsdom',
